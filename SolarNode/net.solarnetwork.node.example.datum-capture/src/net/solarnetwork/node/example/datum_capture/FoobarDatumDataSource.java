7ba1e8971d2a497b74d9d3444ea6918f0034c64e
Matt Magoffin <matt@solarnetwork.net>
2024-07-09 06:56:26 +1200

    Support DatumSourceIdProvider API.

--- SolarNode/net.solarnetwork.node.example.datum-capture/src/net/solarnetwork/node/example/datum_capture/FoobarDatumDataSource.java
+++ SolarNode/net.solarnetwork.node.example.datum-capture/src/net/solarnetwork/node/example/datum_capture/FoobarDatumDataSource.java
@@ -1,21 +1,21 @@
 /* ==================================================================
  * FoobarDatumDataSource.java - 16/08/2016 11:21:55 AM
- * 
+ *
  * Copyright 2007-2016 SolarNetwork.net Dev Team
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation; either version 2 of 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307 USA
  * ==================================================================
  */
@@ -23,6 +23,8 @@
 package net.solarnetwork.node.example.datum_capture;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import net.solarnetwork.domain.datum.DatumSamples;
@@ -36,9 +38,9 @@ import net.solarnetwork.settings.support.BasicTextFieldSettingSpecifier;
 
 /**
  * Implementation of {@link DatumDataSource} for Foobar inverter power.
- * 
+ *
  * @author matt
- * @version 1.0
+ * @version 1.1
  */
 public class FoobarDatumDataSource extends DatumDataSourceSupport
 		implements DatumDataSource, SettingSpecifierProvider {
@@ -56,6 +58,13 @@ public class FoobarDatumDataSource extends DatumDataSourceSupport
 	public FoobarDatumDataSource() {
 		super();
 		setDisplayName("Foobar Datum Source");
+	}
+
+	@Override
+	public Collection<String> publishedSourceIds() {
+		final String sourceId = resolvePlaceholders(getSourceId());
+		return (sourceId == null || sourceId.isEmpty() ? Collections.emptySet()
+				: Collections.singleton(sourceId));
 	}
 
 	@Override
@@ -96,7 +105,7 @@ public class FoobarDatumDataSource extends DatumDataSourceSupport
 
 	/**
 	 * Get the source ID.
-	 * 
+	 *
 	 * @return the configured source ID
 	 */
 	public String getSourceId() {
@@ -105,7 +114,7 @@ public class FoobarDatumDataSource extends DatumDataSourceSupport
 
 	/**
 	 * Set the source ID to use.
-	 * 
+	 *
 	 * @param sourceId
 	 *        the source ID
 	 */
